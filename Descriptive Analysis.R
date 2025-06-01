794366b181f2fb0218d52da2b4ae65d11e7c5eb5
Maruf-Raihan-1997 <maruf.nstu101@gmail.com>
2025-06-01 20:20:02 +0300

    Update Descriptive Analysis.R

---
+++ Descriptive Analysis.R
@@ -0,0 +1,25 @@
+# Descriptive statistics using psych package
+
+install.packages("psych")
+library(psych)
+
+data()
+AirPassengers
+BOD
+
+#Using the New York airport flight data
+
+install.packages("nycflights13")
+attach(nycflights13::flights)
+
+#lets look at the first 6 records using the head function
+
+head(nycflights13::flights)
+
+summary(distance)
+
+describe(distance)
+
+demo = cbind(arr_delay, dep_delay, distance)
+
+describe(demo)
