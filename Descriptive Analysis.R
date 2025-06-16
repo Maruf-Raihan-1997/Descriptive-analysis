c420bfc9a87857254debc04a3882544a8c874486
Maruf-Raihan-1997 <maruf.nstu101@gmail.com>
2025-06-16 23:49:13 +0300

    des

--- Descriptive Analysis.R
+++ Descriptive Analysis.R
@@ -1,6 +1,6 @@
 # Descriptive statistics using psych package
 
-install.packages("psych")
+#install.packages("psych")
 library(psych)
 
 data()
@@ -9,7 +9,7 @@ BOD
 
 #Using the New York airport flight data
 
-install.packages("nycflights13")
+#install.packages("nycflights13")
 attach(nycflights13::flights)
 
 #lets look at the first 6 records using the head function
@@ -23,3 +23,37 @@ describe(distance)
 demo = cbind(arr_delay, dep_delay, distance)
 
 describe(demo)
+
+
+#introducing colnames() function
+
+colnames(demo)= c('Arrival delay','Departure Delay', 'Distance Travelled')
+
+describe(demo)
+
+#hist() function
+
+dep_delay
+
+#we can see a lot of values for departure delay
+
+hist(dep_delay)
+
+# grouping statistics
+
+
+head(nycflights13::flights)
+
+summary(carrier)
+
+carrier_fact= factor(carrier)
+summary(carrier_fact)
+
+describeBy(distance, group = carrier)
+
+
+#two levels of aggrgation
+
+aggregate(distance, by = list(carrier, month) , FUN = mean, na.rm = TRUE)
+
+aggregate(distance, by = list(carrier, month) , FUN = sd, na.rm = TRUE)
